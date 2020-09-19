cd3aaf0cbb9539edcfc03d9a3f7c2dcf7989fa5e
kekeing <851575436@qq.com>
2020-09-19 17:24:07 +0800

    complete factory

---
+++ src/main/java/newargs/SchemaFactory.java
@@ -0,0 +1,17 @@
+package newargs;
+
+public class SchemaFactory {
+
+    public static Schema create(String flag) throws Exception {
+        switch(flag){
+            case "l" :
+                return new Schema(flag,"boolean",false);
+            case "p" :
+                return new Schema(flag,"int",0);
+            case "d" :
+                return new Schema(flag,"String","");
+        }
+        throw new Exception("value不能以-开头");
+    }
+}
+
