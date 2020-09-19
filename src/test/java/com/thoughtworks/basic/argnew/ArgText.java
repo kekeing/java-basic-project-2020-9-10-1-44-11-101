d9e5b1e257c6353d38edc4c3695cd4306ea79c00
kekeing <851575436@qq.com>
2020-09-19 17:26:09 +0800

    add test -1 true and the test passed

--- src/test/java/com/thoughtworks/basic/argnew/ArgText.java
+++ src/test/java/com/thoughtworks/basic/argnew/ArgText.java
@@ -3,7 +3,9 @@ package com.thoughtworks.basic.argnew;
 import newargs.Args;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ArgText {
     @Test
@@ -16,5 +18,16 @@ public class ArgText {
                         "Arg{flag='p  ', value=0, type='int'}, " +
                         "Arg{flag='d ', value=, type='String'}]"
                 ,args.getArgList().toString());
+    }
+    @Test
+    public void should_throw_value_no_legal_when_error_value() throws Exception {
+        String inputArgs = "-l -true";
+
+        try {
+            Args args = new Args(inputArgs);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            assertThat(exception.getMessage(), is("value不能以-开头"));
+        }
     }
 }
