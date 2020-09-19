2834c275db8f99b55109267d56015845d60b6dad
kekeing <851575436@qq.com>
2020-09-19 17:19:57 +0800

    根据ARG新建测试

---
+++ src/test/java/com/thoughtworks/basic/argnew/ArgText.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.basic.argnew;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArgText {
+    @Test
+    public void should_return_defaultValue_when_inputArgs_without_value() throws Exception {
+        String inputArgs = "-l  -p  -d ";
+
+        Args args = new Args(inputArgs);
+
+        assertEquals("[Arg{flag='l  ', value=false, type='boolean'}, " +
+                        "Arg{flag='p  ', value=0, type='int'}, " +
+                        "Arg{flag='d ', value=, type='String'}]"
+                ,args.getArgList().toString());
+    }
+}
